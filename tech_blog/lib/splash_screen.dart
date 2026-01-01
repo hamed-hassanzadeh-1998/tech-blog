884465c5d3bf255d9b0c33323bb104cc64bc5cd1
Hamed <Hamedhssnzdh@gmail.com>
2026-01-02 00:28:38 +0330

    write splash page code and use navigator for it

--- tech_blog/lib/splash_screen.dart
+++ tech_blog/lib/splash_screen.dart
@@ -1,83 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:tech_blog/main_screen.dart';
+import 'package:tech_blog/my_colors.dart';
 
 class SplashScreen extends StatefulWidget {
-  final Widget nextScreen;
-  final Duration duration;
-
-  const SplashScreen({
-    super.key,
-    required this.nextScreen,
-    this.duration = const Duration(seconds: 2),
-  });
+  const SplashScreen({super.key});
 
   @override
   State<SplashScreen> createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen>
-    with SingleTickerProviderStateMixin {
-  late AnimationController _controller;
-  late Animation<double> _fadeAnimation;
-  late Animation<double> _scaleAnimation;
+class _SplashScreenState extends State<SplashScreen> {
 
   @override
   void initState() {
     super.initState();
-
-    _controller = AnimationController(
-      vsync: this,
-      duration: const Duration(milliseconds: 800),
-    );
-
-    _fadeAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(
-      CurvedAnimation(parent: _controller, curve: Curves.easeIn),
-    );
-
-    _scaleAnimation = Tween<double>(begin: 0.8, end: 1.0).animate(
-      CurvedAnimation(parent: _controller, curve: Curves.easeOutBack),
-    );
-
-    _controller.forward();
-
-    Future.delayed(widget.duration, () {
-      if (mounted) {
-        Navigator.of(context).pushReplacement(
-          PageRouteBuilder(
-            pageBuilder: (context, animation, secondaryAnimation) =>
-                widget.nextScreen,
-            transitionsBuilder:
-                (context, animation, secondaryAnimation, child) {
-              return FadeTransition(opacity: animation, child: child);
-            },
-            transitionDuration: const Duration(milliseconds: 500),
-          ),
-        );
-      }
+    Future.delayed(Duration(seconds: 5)).then((value) {
+      Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) => MainScreen()));
     });
+
   }
 
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: Center(
-        child: FadeTransition(
-          opacity: _fadeAnimation,
-          child: ScaleTransition(
-            scale: _scaleAnimation,
-            child: Image.asset(
-              'assets/images/logo.png',
-              width: 250,
-              fit: BoxFit.contain,
-            ),
-          ),
-        ),
+    return SafeArea(
+      child: Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+           Image(image: AssetImage('assets/images/logo.png'),height: 64),
+           SizedBox(height: 32.0),
+           SpinKitFoldingCube(
+            color: SolidColors.primaryColor,
+            size: 32.0,
+           ),
+            ]
+           
+              ),
+        )
       ),
     );
   }
